595c8318a982bda4b9aafe0902dd9d0a5fc0f9ea
Twigg96 <96mhalwig@gmail.com>
2026-04-27 13:19:09 -0400

    Fixed get_files_info

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,15 +4,16 @@ def get_file_content(working_directory, file_path):
     target_dir = os.path.normpath(os.path.join(current, file_path))
     valid_target_dir = os.path.commonpath([current, target_dir]) == current
     if not valid_target_dir:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
-    if os.path.isdir(target_dir) == False:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if os.path.isfile(target_dir) == False:
         return f'Error: File not found or is not a regular file: "{file_path}"'
-    max_chars = 10000
-    with open(file_path, "r") as f:
-        file_content_string = f.read(max_chars)
+    MAX_CHARS = 10000
     try:
-        if f.read(1):
-            content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        with open(target_dir, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return file_content_string
     except Exception as e:
         return f"Error: {str(e)}"
 
